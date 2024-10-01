af7f96253288accf6677fbb24a99a4223b518153
John Paul <johnpaulrayco1@gmail.com>
2024-10-01 14:04:30 +0800

    separate scripts

---
+++ script.js
@@ -0,0 +1,13 @@
+const dialog = document.querySelector('.dialog_log');
+
+function showDialog(){
+    dialog.show();
+}
+
+const hideDialog = () => {
+    dialog.close();
+}
+
+const wutDaDogDoin = () => {
+    console.log('wut the dog doin')
+}
